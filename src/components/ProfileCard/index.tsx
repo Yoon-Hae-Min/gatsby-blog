e355b5838804e24f164634c3328f9446c0a27ea0
YoonHaeMin <yunhatmi@gachon.ac.kr>
2023-06-05 14:45:10 +0900

    feat: ProfileCard 컴포넌트 생성

---
+++ src/components/ProfileCard/index.tsx
@@ -0,0 +1,56 @@
+import { Box, Divider, Heading, Image, Text, useColorMode, useTheme } from '@chakra-ui/react';
+import React from 'react';
+import GithubIcon from '@/assets/svg/github.svg';
+import GmailIcon from '@/assets/svg/gmail.svg';
+import LinkedInIcon from '@/assets/svg/linkedin.svg';
+import ResumeIcon from '@/assets/svg/resume.svg';
+
+const ProfileCard = ({ ...props }) => {
+  const { colorMode } = useColorMode();
+  const theme = useTheme();
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      width="15rem"
+      margin="auto"
+      {...props}
+    >
+      <Image
+        borderRadius="full"
+        boxSize="160px"
+        src="https://avatars.githubusercontent.com/u/49224104?v=4"
+        alt="yoonhaemin profile image"
+      />
+      <Heading size="lg" marginTop="1rem" fontWeight={700}>
+        윤해민
+      </Heading>
+      <Divider marginTop="0.5rem" borderWidth="0.1rem" backgroundColor="white.900" />
+      <Text
+        fontWeight={700}
+        padding="1rem"
+        color={colorMode === 'light' ? 'gray.500' : 'white.800'}
+      >
+        #FE 개발, #React #NextJs #HTML #CSS #Javascript #typescript
+      </Text>
+      <Divider borderWidth="0.1rem" backgroundColor="white.900" />
+      <Box display="flex" columnGap="1.6rem" marginTop="0.9rem">
+        <GithubIcon
+          style={{ fill: colorMode === 'light' ? theme.colors.gray[700] : theme.colors.white[900] }}
+        />
+        <GmailIcon
+          style={{ fill: colorMode === 'light' ? theme.colors.gray[700] : theme.colors.white[900] }}
+        />
+        <LinkedInIcon
+          style={{ fill: colorMode === 'light' ? theme.colors.gray[700] : theme.colors.white[900] }}
+        />
+        <ResumeIcon
+          style={{ fill: colorMode === 'light' ? theme.colors.gray[700] : theme.colors.white[900] }}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default ProfileCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,14 +11,16 @@ import {
 } from '@chakra-ui/react';
 import Header from '../components/Header';
 import SummaryCard from '@/components/SummaryCard';
+import ProfileCard from '@/components/ProfileCard';
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <>
       <main>
-        <Box as="div" display="grid" gridTemplateColumns="1fr 1fr 1fr">
+        <Box as="div" display="grid" gridTemplateColumns="1fr 1fr 1fr" gridRowGap="6rem">
           <SummaryCard title="총 포스트">8일</SummaryCard>
           <SummaryCard title="방문자수">0명</SummaryCard>
+          <ProfileCard gridRow="span 2" />
           <SummaryCard title="테마가 바뀐 횟수">0번</SummaryCard>
           <SummaryCard title="블로그를 시작한지">0일</SummaryCard>
         </Box>
