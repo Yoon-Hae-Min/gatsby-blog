80519ebfd22095b93d916bb00f7cfc8ce5b33f6e
YoonHaeMin <yunhatmi@gachon.ac.kr>
2023-06-20 09:31:26 +0900

    docs: 블로그를 만들자 게시글 작성

--- src/constants/md.ts
+++ src/constants/md.ts
@@ -2,5 +2,6 @@ export const TAG_MAP = {
   review: '회고',
   'technical-experience': '기술 경험',
   'technical-thinking': '기술에 대한 생각',
-  prography: '프로그라피'
+  prography: '프로그라피',
+  'gatsby-blog': '블로그 만들기'
 } as const;
