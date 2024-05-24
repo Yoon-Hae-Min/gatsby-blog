2a7f0ce715cca073e771ce5360dab0f3c4cdade2
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2024-05-24 12:14:05 +0900

    remove: 불필요한 옵션 제거

--- gatsby-config.mjs
+++ gatsby-config.mjs
@@ -165,19 +165,7 @@ const config = {
       }
     },
     {
-      resolve: '@chakra-ui/gatsby-plugin',
-      options: {
-        /**
-         * @property {boolean} [resetCSS=true]
-         * if false, this plugin will not use `<CSSReset />
-         */
-        resetCSS: true,
-        /**
-         * @property {boolean} [isUsingColorMode=true]
-         * if false, this plugin will not use <ColorModeProvider />
-         */
-        isUsingColorMode: true
-      }
+      resolve: '@chakra-ui/gatsby-plugin'
     },
     {
       resolve: 'gatsby-plugin-web-font-loader',
