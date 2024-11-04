c452ff926b57d5984d0e57c5324f2bd70b273135
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2024-11-04 21:48:01 +0900

    test: 사용자 테마 환경 로깅 추가

--- src/components/Header/ToggleThemeButton.tsx
+++ src/components/Header/ToggleThemeButton.tsx
@@ -1,12 +1,25 @@
 import { IconButton, useColorMode, useColorModeValue } from '@chakra-ui/react';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import MoonIcon from '@/assets/svg/moon.svg';
 import SunIcon from '@/assets/svg/sun.svg';
 
 const ToggleThemeButton = () => {
   const ToggleThemeIcon = useColorModeValue(MoonIcon, SunIcon);
-  const { toggleColorMode } = useColorMode();
+  const { toggleColorMode, colorMode } = useColorMode();
+
+  useEffect(() => {
+    const themePreference =
+      window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+        ? 'dark'
+        : 'light';
+
+    window.gtag?.('event', 'theme', {
+      system_theme: themePreference,
+      blog_theme: colorMode
+    });
+  }, [colorMode]);
+
   return (
     <IconButton
       aria-label="toggle-theme-button"
